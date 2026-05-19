dffa0e12ddfcb45218dc8b30481fccc542c48d49
golden-0815 <andreas.larsen.062410@gmail.com>
2026-05-19 15:37:45 +0800

    chore: added price converter file

---
+++ src/PriceConverter.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter{
+
+
+    function getprice( AggregatorV3Interface priceFeed)  internal view  returns(uint256) { 
+         //address ---> 0x694AA1769357215DE4FAC081bf1f309aDC325306
+     (,int256 price,,,)= priceFeed.latestRoundData();
+          return uint256(price * 1e10);
+
+    }
+    function getConversionRate( uint256 ethAmount,
+    AggregatorV3Interface priceFeed
+    ) internal view returns(uint256)  { 
+        uint256 ethPrice = getprice(priceFeed);
+        uint256 ethAmountInUsd = (ethPrice * ethAmount)/1e18; 
+        return ethAmountInUsd; //always multipy first and do devision
+    }
+
+}
